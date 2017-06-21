a525ed9c2dfce1737a1d9991293e36eeb95fb1a3
UsenSatybaldiev <usen.satybaldiev@mail.ru>
2017-06-21 16:40:42 +0600

    1

--- src/com/geekbrains/Main.java
+++ src/com/geekbrains/Main.java
@@ -6,32 +6,32 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        guessNumber();
+    }
+    public static void guessNumber () {
+        System.out.println("Угадайте число от 0 до 9");
         Random random = new Random();
         int v = random.nextInt(10);
         Scanner sc = new Scanner(System.in);
-
-
         for (int i = 0; i < 3; i++) {
+            int t = i + 1;
+            System.out.println("Попытка номер - " + t + ":");
             int n = sc.nextInt();
             if (n == v) {
-                System.out.println("Вы угадали число! Repeat?");
-
+                System.out.println("Вы угадали число!");
+                break;
             }
             else {
                 if (n > v)
-                    System.out.println("Введенное число больше");
+                    System.out.println("Загаданное число меньше указанного");
                 else
-                    System.out.println("Введенное число меньше");
+                    System.out.println("Загаданное число больше указанного");
             }
-
         }
-
-
-
-
-
-//
-//        System.out.println(sc.nextInt());
-
+        System.out.println("Хотите повторить?: Да - 0, Нет - 1");
+        int a = sc.nextInt();
+        if (a == 0) {
+            guessNumber();
+        }
     }
 }
